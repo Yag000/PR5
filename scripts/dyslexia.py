95f55bec76ec9e58fb8096a42174857b3ad6a4b7
Yago Iglesias <yago.iglesias.vazquez@gmail.com>
2024-06-06 15:36:41 +0200

    ci: add building pipeline

---
+++ scripts/dyslexia.py
@@ -0,0 +1,6 @@
+with open("questions.tex", "r") as f:
+    lines = f.readlines()
+    lines.insert(3, "\\usepackage{fontspec}\n")
+    lines.insert(4, "\\setmainfont{OpenDyslexic}\n")
+with open("questions.tex", "w") as f:
+    f.writelines(lines)
