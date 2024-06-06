53a359d81ea74eb9bb8558887f7eaa1d86464a00
Yago Iglesias <yago.iglesias.vazquez@gmail.com>
2024-06-06 15:46:44 +0200

    fix: generate dyslexia document

--- scripts/dyslexia.py
+++ scripts/dyslexia.py
@@ -2,5 +2,5 @@ with open("questions.tex", "r") as f:
     lines = f.readlines()
     lines.insert(3, "\\usepackage{fontspec}\n")
     lines.insert(4, "\\setmainfont{OpenDyslexic}\n")
-with open("questions.tex", "w") as f:
+with open("questions-dyslexia.tex", "w") as f:
     f.writelines(lines)
